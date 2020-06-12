5aca4f64ad3f8e665245fa6e51960e1afd87bb52
pramodpatwa11986 <pramod_patwa2003@yahoo.com>
2020-06-12 12:38:50 +0530

    client aith commit

--- src/main/java/com/mywork/secure/socket/demo/JavaSSLClient.java
+++ src/main/java/com/mywork/secure/socket/demo/JavaSSLClient.java
@@ -1,14 +1,26 @@
 package com.mywork.secure.socket.demo;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.KeyStore;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+
+import com.sun.net.ssl.KeyManager;
 
 /**
  * @web http://java-buddy.blogspot.com/
@@ -20,7 +32,8 @@ public class JavaSSLClient {
     public static void main(String[] args) {
         
         SSLSocketFactory sslSocketFactory = 
-                (SSLSocketFactory)SSLSocketFactory.getDefault();
+                //(SSLSocketFactory)SSLSocketFactory.getDefault();
+        		getClientSocketFactory();
         try {
             Socket socket = sslSocketFactory.createSocket("localhost", port);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -46,6 +59,48 @@ public class JavaSSLClient {
         }
          
     }
+    
+    private static SSLSocketFactory getClientSocketFactory() {
+		SSLSocketFactory ssf = null;
+		try {
+			// set up key manager to do server authentication
+			SSLContext ctx;
+			KeyManagerFactory kmf;
+			KeyStore ks;
+			KeyStore trustStore;
+			char[] passphrase = "123456".toCharArray();
+
+			ctx = SSLContext.getInstance("TLS");
+			kmf = KeyManagerFactory.getInstance("SunX509");
+			ks = KeyStore.getInstance("JKS");
+
+			ks.load(new FileInputStream(
+					new File("D:\\project\\eclipse-workspace\\secure-socket-demo\\cert-poc\\client-keystore.jks")), passphrase);
+			kmf.init(ks, passphrase);
+			
+			
+			TrustManagerFactory tmFactory = TrustManagerFactory.getInstance("SunX509");
+            //Adds the truststore to the factory
+			trustStore = KeyStore.getInstance("JKS");
+
+			trustStore.load(new FileInputStream(
+					new File("D:\\project\\eclipse-workspace\\secure-socket-demo\\cert-poc\\server.jks")), passphrase);
+            tmFactory.init(trustStore);
+            //This is passed to the SSLContext init method
+            TrustManager[] managers = tmFactory.getTrustManagers();
+			
+			System.out.println("Keymanagers***** " + kmf.getKeyManagers());
+			
+			ctx.init(kmf.getKeyManagers(), managers, null);
+
+			ssf = ctx.getSocketFactory();
+			
+			return ssf;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+}
     
 }
 
--- src/main/java/com/mywork/secure/socket/demo/JavaSSLServer.java
+++ src/main/java/com/mywork/secure/socket/demo/JavaSSLServer.java
@@ -1,13 +1,21 @@
 package com.mywork.secure.socket.demo;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyStore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 
 /**
@@ -16,16 +24,19 @@ import javax.net.ssl.SSLServerSocketFactory;
 public class JavaSSLServer {
     
     static final int port = 8000;
-
+    
+    
     public static void main(String[] args) {
         
         
-        SSLServerSocketFactory sslServerSocketFactory = 
-                (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
-        
+        //SSLServerSocketFactory sslServerSocketFactory = 
+          //      (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+    	SSLServerSocketFactory sslServerSocketFactory = 
+    			(SSLServerSocketFactory)getServerSocketFactory();    	
         try {
             ServerSocket sslServerSocket = 
                     sslServerSocketFactory.createServerSocket(port);
+           // ((SSLServerSocket) sslServerSocket).setNeedClientAuth(true);
             System.out.println("SSL ServerSocket started");
             System.out.println(sslServerSocket.toString());
             
@@ -49,5 +60,32 @@ public class JavaSSLServer {
             Logger.getLogger(JavaSSLServer.class.getName())
                     .log(Level.SEVERE, null, ex);
         }
-    }    
+    }
+    
+    private static ServerSocketFactory getServerSocketFactory() {
+			SSLServerSocketFactory ssf = null;
+			try {
+				// set up key manager to do server authentication
+				SSLContext ctx;
+				KeyManagerFactory kmf;
+				KeyStore ks;
+				char[] passphrase = "123456".toCharArray();
+
+				ctx = SSLContext.getInstance("TLS");
+				kmf = KeyManagerFactory.getInstance("SunX509");
+				ks = KeyStore.getInstance("JKS");
+
+				ks.load(new FileInputStream(
+						new File("D:\\project\\eclipse-workspace\\secure-socket-demo\\cert-poc\\server.jks")), passphrase);
+				kmf.init(ks, passphrase);
+				
+				ctx.init(kmf.getKeyManagers(), null, null);
+
+				ssf = ctx.getServerSocketFactory();			
+				return ssf;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+	}
 }
